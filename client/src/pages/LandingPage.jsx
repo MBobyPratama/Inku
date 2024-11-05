350c572f4828c496ba3b011ca3267f9567006fbd
SulthanYasin <sulthanzain28@gmail.com>
2024-11-05 20:32:53 +0700

    Fix import path for background image in LandingPage component

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import NavBar from "../components/Navbar";
 import Banner from "../components/Banner";
 import LoginCard from "../components/LoginCard";
 import BenefitCard from "../components/BenefitCard";
-import Background from "../assets/background.jpg";
+import Background from "../assets/Background.jpg";
 import Footer from "../components/footer";
 import Signup from "../components/signup";
 import { useState } from "react";
