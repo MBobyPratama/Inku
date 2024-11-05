e03b0c87d73b8104e693e882f9525b198985e0db
SulthanYasin <sulthanzain28@gmail.com>
2024-11-05 20:34:20 +0700

    Remove LoginCard and Signup components from the project

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,13 +1,10 @@
 import NavBar from "../components/Navbar";
 import Banner from "../components/Banner";
-import LoginCard from "../components/LoginCard";
 import BenefitCard from "../components/BenefitCard";
 import Background from "../assets/Background.jpg";
 import Footer from "../components/footer";
-import Signup from "../components/signup";
 import { useState } from "react";
 export default function LandingPage() {
-  const [login, setLogin] = useState(true);
   return (
     <div style={{ backgroundImage: `url(${Background})` }}>
       <NavBar />
