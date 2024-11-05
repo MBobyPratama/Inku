350366a97e7371df2d41bb3655f6385f8fbcfa0a
SulthanYasin <sulthanzain28@gmail.com>
2024-11-05 20:35:24 +0700

    Fix import case sensitivity for Logo asset in NavBar2 and remove unused useState import in LandingPage

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,7 +3,6 @@ import Banner from "../components/Banner";
 import BenefitCard from "../components/BenefitCard";
 import Background from "../assets/Background.jpg";
 import Footer from "../components/footer";
-import { useState } from "react";
 export default function LandingPage() {
   return (
     <div style={{ backgroundImage: `url(${Background})` }}>
